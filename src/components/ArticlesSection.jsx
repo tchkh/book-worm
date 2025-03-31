a723072ef1f9ca771f040c8c2dedeed474f18d00
Jay-Asalways <>
2025-04-01 00:19:45 +0700

    feat: make articles section interact with user by using state

--- src/components/ArticlesSection.jsx
+++ src/components/ArticlesSection.jsx
@@ -7,8 +7,53 @@ import {
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/select'
+import { blogPosts } from '@/data/blogPosts'
+import authorImage from '../assets/author-image.jpg'
+import { useState } from 'react'
+
+function BlogCard({ post }) {
+  return (
+    <div className='flex flex-col gap-4'>
+      <a href='#' className='relative h-[212px] sm:h-[360px]'>
+        <img
+          className='w-full h-full object-cover rounded-md'
+          src={post.image}
+          alt={post.title}
+        />
+      </a>
+      <div className='flex flex-col'>
+        <div className='flex'>
+          <span className='bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-green-600 mb-2'>
+            {post.category}
+          </span>
+        </div>
+        <a href='#'>
+          <h2 className='font-bold text-xl mb-2 line-clamp-2 hover:underline'>
+            {post.title}
+          </h2>
+        </a>
+        <p className='text-muted-foreground text-sm mb-4 flex-grow line-clamp-3'>
+          {post.description}
+        </p>
+        <div className='flex items-center text-sm'>
+          <img
+            className='w-8 h-8 rounded-full mr-2'
+            src={authorImage}
+            alt={post.author}
+          />
+          <span>{post.author}</span>
+          <span className='mx-2 text-gray-300'>|</span>
+          <span>{post.date}</span>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 function Articles() {
+  const categories = ['Highlight', 'Cat', 'Inspiration', 'General']
+  const [category, setCategory] = useState('Highlight')
+
   return (
     <div className='w-full max-w-7xl mx-auto md:px-6 lg:px-8 mb-10'>
       <h2 className='text-xl font-bold mb-4 px-4'>Latest articles</h2>
@@ -24,8 +69,8 @@ function Articles() {
           </div>
         </div>
         <div className='md:hidden w-full'>
-          <Select value='highlight'>
-            <SelectTrigger className='w-full py-3 rounded-sm text-muted-foreground'>
+          <Select value={category} onValueChange={value => setCategory(value)}>
+            <SelectTrigger className='w-full py-3 rounded-sm text-muted-foreground focus:ring-0 focus:ring-offset-0 focus:border-muted-foreground'>
               <SelectValue placeholder='Select category' />
             </SelectTrigger>
             <SelectContent>
@@ -37,30 +82,18 @@ function Articles() {
           </Select>
         </div>
         <div className='hidden md:flex space-x-2'>
-          <button
-            className='px-4 py-3 transition-colors rounded-sm text-sm text-muted-foreground font-medium bg-[#DAD6D1]
-             '
-          >
-            Highlight
-          </button>
-          <button
-            className='px-4 py-3 transition-colors rounded-sm text-sm text-muted-foreground font-medium bg-[muted]
-             '
-          >
-            Cat
-          </button>
-          <button
-            className='px-4 py-3 transition-colors rounded-sm text-sm text-muted-foreground font-medium bg-[muted]
-             '
-          >
-            Inspiration
-          </button>
-          <button
-            className='px-4 py-3 transition-colors rounded-sm text-sm text-muted-foreground font-medium bg-[muted]
-             '
-          >
-            General
-          </button>
+          {categories.map(tag => (
+            <button
+              key={tag}
+              onClick={() => setCategory(tag)}
+              disabled={category === tag}
+              className={`px-4 py-3 transition-colors rounded-sm text-sm text-muted-foreground font-medium ${
+                category === tag ? 'bg-[#DAD6D1]' : 'hover:bg-muted'
+              }`}
+            >
+              {tag}
+            </button>
+          ))}
         </div>
       </div>
     </div>
