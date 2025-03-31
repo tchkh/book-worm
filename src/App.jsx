054df28530d769d17efe59beac0c2cfbed844261
Jay-Asalways <>
2025-03-31 20:25:52 +0700

    feat: add footer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import Articles from './components/ArticlesSection'
+import Footer from './components/Footer'
 import HeroSection from './components/HeroSection'
 import NavBar from './components/NavBar'
 
 function App() {
   return (
-    <>
+    <div className='flex flex-col min-h-screen'>
       <NavBar />
-      <HeroSection />
-      <Articles />
-    </>
+      <div className='flex-grow'>
+        <HeroSection />
+        <Articles />
+      </div>
+      <Footer />
+    </div>
   )
 }
 
